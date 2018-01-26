e7f1a011db882aa3b5a1c65ab18a2b46adf30492
itachi9214 <diakhateserignesaliou@gmail.com>
2018-01-26 00:20:52 -0500

    new Branche

--- MQT-6014/src/tp1/MyFirstTP.java
+++ MQT-6014/src/tp1/MyFirstTP.java
@@ -2,4 +2,33 @@ package tp1;
 
 public class MyFirstTP {
 
+  public static void main(String[] args) {
+
+    // code pour appeler la fonction d)
+    double a = 2.3, b = 5.2, c = 7.3;
+
+    // code pour appeler la fonction e)
+    double n1 = 13, n2 = 27, n3 = 53, n4 = 28;
+
+    // code pour appeler la fonction f)
+    int nf = 10;
+
+    // code pour appeler la fonction g)
+    int g_n = 25;
+
+  }
+  // Développement de a)
+
+  // Développement de b)
+
+  // Explication de c)
+
+  // Fonction pour le numéro d)
+
+  // Fonction pour le numéro e)
+
+  // Fonction pour le numéro f)
+
+  // Fonction pour le numéro g)
+
 }
