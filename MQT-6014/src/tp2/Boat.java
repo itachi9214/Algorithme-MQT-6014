60bdf4bd18483b20f80ac243edd0ba15715959de
itachi9214 <diakhateserignesaliou@gmail.com>
2018-02-14 23:12:47 -0500

    Batleship started

---
+++ MQT-6014/src/tp2/Boat.java
@@ -0,0 +1,52 @@
+package tp2;
+
+public class Boat {
+
+  private int abscissa;
+  private int ordinate;
+  private boolean statutBoat;
+
+  public Boat(int abscissa, int ordinate) {
+    super();
+    this.abscissa = abscissa;
+    this.ordinate = ordinate;
+    this.statutBoat = false;
+  }
+
+  public int getAbscissa() {
+    return abscissa;
+  }
+
+  public void setAbscissa(int abscissa) {
+    this.abscissa = abscissa;
+  }
+
+  public int getOrdinate() {
+    return ordinate;
+  }
+
+  public void setOrdinate(int ordinate) {
+    this.ordinate = ordinate;
+  }
+
+  public boolean isSunk() {
+    return statutBoat;
+  }
+
+  public void setStatutBoat(boolean statutBoat) {
+    this.statutBoat = statutBoat;
+  }
+
+  public boolean isTouch(int abscissa, int ordinate) {
+    if (isSunk()) {
+      System.out.println("Bateau deja touché");
+      return true;
+    } else if (this.abscissa == abscissa && this.ordinate == ordinate) {
+      setStatutBoat(true);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
---
+++ MQT-6014/src/tp2/Game.java
@@ -0,0 +1,19 @@
+package tp2;
+
+import java.util.ArrayList;
+
+public class Game {
+
+  private static final int NUMBER_OF_BOAT = 4;
+  private static final int DIMENSION_ABSCISSA = 4;
+  private static final int DIMENSION_ORDINATE = 4;
+
+  private ArrayList<Boat> listOfBoat;
+
+  public static void main(String[] args) {
+    Util util = new Util();
+    util.getNumber(4, 4);
+
+  }
+
+}
