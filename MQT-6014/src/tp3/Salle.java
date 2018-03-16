f6866ec86c67e6a13941e9ba47fc7ebd2895a1a4
itachi9214 <diakhateserignesaliou@gmail.com>
2018-03-15 23:44:32 -0400

    salle ready

--- MQT-6014/src/tp3/Salle.java
+++ MQT-6014/src/tp3/Salle.java
@@ -1,5 +1,41 @@
 package tp3;
 
+import java.util.ArrayList;
+
 public class Salle {
+
+  private double sumOfAverage = 0.0;
+  private double sumOfVariance = 0.0;
+  private double deviation = 0.0;
+  private ArrayList<Operation> listOperations;
+
+  public double getSumOfAverage() {
+    for (int i = 0; i < listOperations.size(); i++)
+      sumOfAverage += listOperations.get(i).getAverage();
+    return sumOfAverage;
+  }
+
+  public void setSumOfAverage(double sumOfAverage) {
+    this.sumOfAverage = sumOfAverage;
+  }
+
+  public double getSumOfVariance() {
+    for (int i = 0; i < listOperations.size(); i++)
+      sumOfVariance += listOperations.get(i).getVariance();
+    return sumOfVariance;
+  }
+
+  public void setSumOfVariance(double sumOfVariance) {
+    this.sumOfVariance = sumOfVariance;
+  }
+
+  public double getDeviation() {
+    deviation = Math.sqrt(sumOfVariance);
+    return deviation;
+  }
+
+  public void setDeviation(double deviation) {
+    this.deviation = deviation;
+  }
 
 }
